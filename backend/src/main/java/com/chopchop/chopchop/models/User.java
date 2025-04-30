bb15973de58cec36eaa19a3486d1b02d27d47e9f
Tavini2002 <taviniperera2002@gmail.com>
2025-04-30 13:00:45 +0530

    comment option

--- backend/src/main/java/com/chopchop/chopchop/models/User.java
+++ backend/src/main/java/com/chopchop/chopchop/models/User.java
@@ -1,41 +1,28 @@
 package com.chopchop.chopchop.models;
-
 import lombok.*;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.*;
-
+import java.util.Collections;
+import java.util.List;
+import org.springframework.data.mongodb.core.index.Indexed;
 @Document(collection = "users")
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Setter
+@Getter
 public class User implements UserDetails {
 
     @Id
     private String id;
 
-    @Indexed(unique = true)
     private String username;
 
     private String password;
-    private String fullName;
-    private String email;
-    private String avatar; // URL to profile image
-    private String bio;
 
-    private List<String> followers = new ArrayList<>();
-    private List<String> following = new ArrayList<>();
-
-    private List<Notification> notifications = new ArrayList<>();
-
-    // Spring Security methods
     @Override
-    public Collection getAuthorities() {
-        return Collections.emptyList();
+    public List getAuthorities() {
+        return Collections.EMPTY_LIST;
     }
 
     @Override
